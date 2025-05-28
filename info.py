106cf428952a723fab7ccc381e774bfcd332f756
ledass <nistalieocs@gmail.com>
2025-05-28 10:49:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,10 +76,10 @@ TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', ""mongodb+srv://elsa2008:elsa2008@cluster0.nydncsj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://elsa2008:elsa2008@cluster0.pz39mgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://elsa2008:elsa2008@cluster0.nydncsj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://elsa2008:elsa2008@cluster0.pz39mgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 #Downloader
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
